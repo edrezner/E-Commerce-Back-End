45efd5d4e818ea68598afddf6e379becba3533bf
Seven <edrezner7@gmail.com>
2023-05-26 21:50:31 -0400

    first commit and started setting up relationships and model data structure

---
+++ models/Product.js
@@ -0,0 +1,59 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection");
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+    },
+
+    // stock
+
+    // Integer
+
+    // Doesn't allow null values
+
+    // Set a default value of 10
+
+    // Validates that the value is numeric
+
+    // category_id
+
+    // Integer
+
+    // References the category model's id
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product",
+  }
+);
+
+module.exports = Product;
